6153916557d591eaadfd2dac736a2a0cf7f77539
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-06-27 18:22:49 -0500

    small changes

--- server/lib/models/template.js
+++ server/lib/models/template.js
@@ -1,44 +1,56 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const templateSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 50,
-        trim: true,
-    },
-    summary: {
-        type: String,
-        required: false,
-        minlength: 1,
-        maxlength: 400,
-        trim: false,
-    },
-    templateData: {
-        type: Object,
-        required: true
-    }
-},
-{
-    toJSON: {
-        transform: function (doc, obj) {
-            obj.id = obj._id;
-            delete obj._id;
-            delete obj.__v;
+const templateSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			minlength: 1,
+			maxlength: 50,
+			trim: true,
+		},
+		summary: {
+			type: String,
+			required: false,
+			minlength: 1,
+			maxlength: 400,
+			trim: false,
+		},
+		templateData: {
+			type: Object,
+			required: true,
+		},
+	},
+	{
+		toJSON: {
+			transform: function (doc, obj) {
+				obj.id = obj._id;
+				delete obj._id;
+				delete obj.__v;
+			},
+		},
+	}
+);
+
+templateSchema.pre("save", function (next) {
+	const template = this; // only works because this _isn't_ an arrow function. Gotta love JS.
+	console.log(template);
+	// TODO: Check to make sure templateData is valid, recursively
+	//       throw error if not valid
+	try {
+		if (typeof template !== object) {
+			throw new Error("template not an object");
+		}
+
+        if (template.container) {
+            validateContainer(container)
         }
-    }
+	} catch (err) {
+		console.log("Error", err);
+	}
+	next();
 });
 
-templateSchema.pre('save', function (next) {
-    const template = this; // only works because this _isn't_ an arrow function. Gotta love JS.
-    console.log(template)
-    // TODO: Check to make sure templateData is valid, recursively
-    //       throw error if not valid
-    next();
-});
-
-const Template = mongoose.model('Template', templateSchema);
+const Template = mongoose.model("Template", templateSchema);
 
 export default Template;
-
